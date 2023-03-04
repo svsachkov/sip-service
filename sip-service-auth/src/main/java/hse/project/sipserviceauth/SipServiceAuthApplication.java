0c7db6a8ffd643092d47d03f8c067bbcb97b2372
david-arutyunyan <daarutyunyan@edu.hse.ru>
2023-03-04 15:21:21 +0300

    init auth service

---
+++ sip-service-auth/src/main/java/hse/project/sipserviceauth/SipServiceAuthApplication.java
@@ -0,0 +1,13 @@
+package hse.project.sipserviceauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipServiceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipServiceAuthApplication.class, args);
+	}
+
+}
